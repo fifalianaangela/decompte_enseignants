91654717d034bb7383bb9d2ece3d8e5ff552ce98
Fifaliana <fifaliana.angela@gmail.com>
2024-09-06 07:59:10 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ContainerComponents from './components/ContainerComponents';
+// import Enseignant from './pages/enseignants/Enseignant';
+
+function App() {
+  return (
+    <>
+      <ContainerComponents/>
+    </>
+  );
+}
+
+export default App;
